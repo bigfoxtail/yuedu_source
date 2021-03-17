21c5be245ca18eb5191920e27d260a48947629d3
bigfoxtail <bigfoxtail@outlook.com>
2021-03-17 20:04:55 +0800

    '修改源地址'

--- makefile.py
+++ makefile.py
@@ -5,11 +5,23 @@ import demjson
 import requests
 
 source_list = [
-    "https://xiu2.github.io/yuedu/shuyuan",
+    "https://yuedu.xiu2.xyz/shuyuan",
+    # "https://xiu2.github.io/yuedu/shuyuan",
     "http://alanskycn.gitee.io/vip/assets/import/book_source.json",
-    "https://gitee.com/zmn1307617161/booksource/raw/master/%E4%B9%A6%E6%BA%90/%E7%B2%BE%E6%8E%923.txt",
-    "https://gitee.com/haobai1/bookyuan/raw/master/shuyuan.json",
+    # "https://gitee.com/zmn1307617161/booksource/raw/master/%E4%B9%A6%E6%BA%90/%E7%B2%BE%E6%8E%923.txt",
+    "https://guaner001125.coding.net/p/coding-code-guide/d/booksources/git/raw/master/sources/guaner.json",
+    # "https://gitee.com/haobai1/bookyuan/raw/master/shuyuan.json",
     "https://gitee.com/slght/yuedu_booksource/raw/master/%E4%B9%A6%E6%BA%90/API%E4%B9%A6%E6%BA%90_3.0.json",
+    "https://www.lifves.com/api/v2/booksource/list",
+    "http://shuyuan.miaogongzi.site/shuyuan/1614645247.json",
+    "http://shuyuan.miaogongzi.site/shuyuan/1614644339.json",
+    "https://raw.githubusercontent.com/CNAD666/MyData/master/novel/bookshelf/N个书源(长期更新).txt",
+    "https://raw.githubusercontent.com/CNAD666/MyData/master/novel/bookshelf/768个校对书源.txt",
+    "https://raw.githubusercontent.com/CNAD666/MyData/master/novel/bookshelf/54个优质书源(14个发现).xml",
+    "https://raw.githubusercontent.com/CNAD666/MyData/master/novel/bookshelf/14个发现书源.txt",
+    "https://gitlab.com/GJTQQ/YueDu/raw/master/Share/915.json",
+    "https://gitlab.com/GJTQQ/YueDu/raw/master/Share/425.json",
+    "https://gitlab.com/GJTQQ/YueDu/raw/master/Share/WxSource.json",
 ]
 
 headers = {
