cf043911f616962d56c8a901f69286edc04c88f2
bigfox <bigfoxtail@outlook.com>
2021-11-24 00:29:40 +0800

    更新

---
+++ clear.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import json
+
+source_content = []
+new_source_content = []
+new_RSS_content = []
+SourceList = {}
+
+with open("yuedu_source.json", 'r', encoding='utf-8') as file_to_read:
+    resjson = json.loads(file_to_read.read())
+    source_content.extend(resjson)
+
+for d in source_content:
+    bookSourceName = d.get("bookSourceName")
+    bookSourceGroup = d.get("bookSourceGroup")
+    bookSourceUrl = d.get("bookSourceUrl")
+
+    FindFlage = "N"
+    SearchFlage = "N"
+    aa = d.get("searchUrl")
+    if d.get("ruleFindUrl") is not None:
+        FindFlage = "Y"
+    if d.get("ruleSearchUrl") is not None or d.get("searchUrl") is not None:
+        SearchFlage = "Y"
+    OnlyFindFlage = "N"
+    if FindFlage == "Y" and SearchFlage == "N":
+        OnlyFindFlage = "Y"
+
+    if OnlyFindFlage == "Y" or "仅发现" in str(bookSourceName) or "仅发现" in str(bookSourceGroup):
+        d["bookSourceGroup"] = d["bookSourceGroup"] + ";仅发现组"
+    if bookSourceUrl is not None:
+        d["bookSourceUrl"] = bookSourceUrl.strip()
+        new_source_content.append(d)
+        if SourceList.get(bookSourceUrl.strip()) is None:
+            SourceList[bookSourceUrl.strip()] = d
+        else:
+            if SourceList[bookSourceUrl.strip()].get("lastUpdateTime", 0) < d.get("lastUpdateTime", 0):
+                SourceList[bookSourceUrl.strip()] = d
+    else:
+        new_RSS_content.append(d)
+    # print(bookSourceName, bookSourceGroup, bookSourceUrl, "发现", FindFlage, "搜索", SearchFlage, "仅搜索", OnlyFindFlage)
+
+SourceList = list(SourceList.values())
+
+f = "new_yuedu_source.json"
+with open(f, "w") as file:
+    file.write(json.dumps(new_source_content))
+f = "new_RSS_source.json"
+with open(f, "w") as file:
+    file.write(json.dumps(new_RSS_content))
+f = "clear_yuedu_source.json"
+with open(f, "w") as file:
+    file.write(json.dumps(SourceList))
--- makefile.py
+++ makefile.py
@@ -1,27 +1,79 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import json
-import demjson
+# import demjson 效率极低，早期为了应对非标json使用
 import requests
 
+
+def delete_duplicate_str(data):
+    immutable_dict = set([str(item) for item in data])
+    data = [eval(i) for i in immutable_dict]
+    return data
+
+
 source_list = [
-    "https://yuedu.xiu2.xyz/shuyuan",
-    # "https://xiu2.github.io/yuedu/shuyuan",
-    "http://alanskycn.gitee.io/vip/assets/import/book_source.json",
-    # "https://gitee.com/zmn1307617161/booksource/raw/master/%E4%B9%A6%E6%BA%90/%E7%B2%BE%E6%8E%923.txt",
-    "https://guaner001125.coding.net/p/coding-code-guide/d/booksources/git/raw/master/sources/guaner.json",
-    # "https://gitee.com/haobai1/bookyuan/raw/master/shuyuan.json",
-    "https://gitee.com/slght/yuedu_booksource/raw/master/%E4%B9%A6%E6%BA%90/API%E4%B9%A6%E6%BA%90_3.0.json",
-    "https://www.lifves.com/api/v2/booksource/list",
-    "http://shuyuan.miaogongzi.site/shuyuan/1614645247.json",
-    "http://shuyuan.miaogongzi.site/shuyuan/1614644339.json",
-    "https://raw.githubusercontent.com/CNAD666/MyData/master/novel/bookshelf/N个书源(长期更新).txt",
-    "https://raw.githubusercontent.com/CNAD666/MyData/master/novel/bookshelf/768个校对书源.txt",
-    "https://raw.githubusercontent.com/CNAD666/MyData/master/novel/bookshelf/54个优质书源(14个发现).xml",
-    "https://raw.githubusercontent.com/CNAD666/MyData/master/novel/bookshelf/14个发现书源.txt",
+    # RSS 官方源 不好用 混起来会有问题 https://gitee.com/alanskycn/yuedu/raw/master/JS/RSS/custom/customRss.json",
+
+    # RSS
+    "https://shuyuan.miaogongzi.net/shuyuan/1633030418.json",
+
+    "https://github.com/xdd666t/MyData/raw/master/novel/bookshelf/14个发现书源.txt",
+    "https://github.com/xdd666t/MyData/raw/master/novel/bookshelf/54个优质书源(14个发现).xml",
+    "https://github.com/xdd666t/MyData/raw/master/novel/bookshelf/768个校对书源.txt",
+    "https://github.com/xdd666t/MyData/raw/master/novel/bookshelf/868个分组书源(逐个校对的).txt",
+    "https://github.com/xdd666t/MyData/raw/master/novel/bookshelf/N个书源(长期更新).txt",
+    "https://github.com/xdd666t/MyData/raw/master/novel/bookshelf/N个优质书源.txt",
+
+    "https://github.com/xdd666t/MyData/raw/master/novel/read3.0/N_booksource.json",
+    "https://github.com/xdd666t/MyData/raw/master/novel/read3.0/nice_booksource.json",
+
+    "https://github.com/xdd666t/MyData/raw/master/novel/155个自留优质书源（不含H）.txt",
+    "https://github.com/xdd666t/MyData/raw/master/novel/1700个自用书源.json",
+    "https://github.com/xdd666t/MyData/raw/master/novel/484个优质书源.txt",
+    "https://github.com/xdd666t/MyData/raw/master/novel/66个精品书源.json",
+
     "https://gitlab.com/GJTQQ/YueDu/raw/master/Share/915.json",
     "https://gitlab.com/GJTQQ/YueDu/raw/master/Share/425.json",
     "https://gitlab.com/GJTQQ/YueDu/raw/master/Share/WxSource.json",
+    "https://gitlab.com/GJTQQ/YueDu/raw/master/Share/雨则10.16二次校验_854个书源_.json",
+
+    "https://www.lifves.com/api/v2/booksource/list",
+
+    "https://gedoor.github.io/MyBookshelf/bookSource.json",
+    "http://www.legado.top/MyBookshelf/bookSource.json",
+
+    "https://yuedu.xiu2.xyz/shuyuan",
+
+    "http://no-mystery.gitee.io/shuyuan/全网通用.json",
+
+    "https://raw.githubusercontent.com/XIU2/Yuedu/master/shuyuan",
+
+    "http://alanskycn.gitee.io/vip/assets/import/book_source.json",
+    "http://alanskycn.gitee.io/vip/assets/import/bs.json",
+    "https://gitee.com/slght/yuedu_booksource/raw/master/书源/API书源_3.0.json",
+
+    # 失效 "https://celeter.github.io/SourceGo/book_source/all.json",
+
+    "http://alanskycn.gitee.io/vip/assets/import/audio_source.json",
+
+    "https://cdn.jsdelivr.net/gh/yeyulingfeng01/yuedu.github.io@1.1/202003.txt",
+    "https://cdn.jsdelivr.net/gh/yeyulingfeng01/yuedu.github.io/yeudu3.0-202005.json",
+
+    "https://moonbegonia.github.io/Source/yuedu/audio.json",
+
+    "https://moonbegonia.github.io/Source/yuedu/fullSourceIncludeInvalid.json",
+
+    # 失效 "https://xiu2.github.io/yuedu/shuyuan",
+
+    # 失效 "https://gitee.com/zmn1307617161/booksource/raw/master/书源/精排3.txt",
+    "https://guaner001125.coding.net/p/coding-code-guide/d/booksources/git/raw/master/sources/guaner.json",
+    "https://gitee.com/haobai1/bookyuan/raw/master/shuyuan.json",
+
+    "http://shuyuan.miaogongzi.site/shuyuan/1614645247.json",
+    "http://shuyuan.miaogongzi.site/shuyuan/1614644339.json",
+
+    "https://shuyuan.miaogongzi.net/shuyuan/1633024206.json",
+    "http://www.yckceo.com/d/y1CBQ",
 ]
 
 headers = {
@@ -36,16 +88,34 @@ headers = {
 }
 
 source_content = []
+
+# 手动写死算了 省事 谁知道以后要不要单独处理
+with open("book\BookSource.json", 'r', encoding='utf-8') as file_to_read:
+    resjson = json.loads(file_to_read.read().replace('\n', ""))
+    source_content.extend(resjson)
+with open("book\OldBookSource.json", 'r', encoding='utf-8') as file_to_read:
+    resjson = json.loads(file_to_read.read().replace('\n', ""))
+    source_content.extend(resjson)
+with open("book\RSSSource.json", 'r', encoding='utf-8') as file_to_read:
+    resjson = json.loads(file_to_read.read().replace('\n', ""))
+    source_content.extend(resjson)
+
 for url in source_list:
     try:
         rs = requests.session()
         res = rs.get(url, headers=headers)
         res.encoding = 'utf-8'
-        resjson = demjson.decode(res.text)
+        resjson = json.loads(res.text.replace('\n', ""))
         source_content.extend(resjson)
+        print("下载成功", url)
     except Exception as e:
+        print("下载错误", url)
+        print('repr(e):\t', repr(e))
         pass
 
 f = "yuedu_source.json"
-with open(f, "w") as file:  # ”w"代表着每次运行都覆盖内容
-    file.write(json.dumps(source_content))
+with open(f, "w") as file:
+    content = json.dumps(source_content, sort_keys=True)
+    content = json.loads(content)
+    content = delete_duplicate_str(content)
+    file.write(json.dumps(content))
